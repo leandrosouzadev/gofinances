df84bc023f4379f88cd3b4de98eaed10295be963
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2021-10-02 08:10:10 -0300

    Validação de formulário

--- src/components/Form/InputForm/index.tsx
+++ src/components/Form/InputForm/index.tsx
@@ -4,16 +4,21 @@ import { Control, Controller } from 'react-hook-form';
 
 import { Input } from '../Input';
 
-import { Container } from './styles';
+import {
+    Container,
+    Error
+} from './styles';
 
 interface Props extends TextInputProps {
     control: Control;
     name: string;
+    error: string;
 }
 
 export function InputForm({
     control,
     name,
+    error,
     ...rest
 }: Props) {
     return (
@@ -29,7 +34,9 @@ export function InputForm({
                 )}
 
                 name={name}
-            />                   
+            />
+
+            {error && <Error>{error}</Error>}
         </Container>
     );
 }
