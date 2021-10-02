6d287050dd5df368e284c87a9e381c13e7b595bf
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2021-10-02 09:20:11 -0300

    Configurações de navegação

--- App.tsx
+++ App.tsx
@@ -10,6 +10,8 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
+
+import { Dashboard } from './src/screens/Dashboard';
 import { Register } from './src/screens/Register';
 
 export default function App() {
