620331b89275fc1e82bf17573c9642e7e718ce78
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2021-10-02 10:14:37 -0300

    Utilizando o tab navitation

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 
+import { NavigationContainer } from '@react-navigation/native';
+
 import {
   useFonts,
   Poppins_400Regular,
@@ -11,8 +13,7 @@ import {
 
 import theme from './src/global/styles/theme';
 
-import { Dashboard } from './src/screens/Dashboard';
-import { Register } from './src/screens/Register';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +28,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
--- android/app/src/main/java/com/gofinances/MainActivity.java
+++ android/app/src/main/java/com/gofinances/MainActivity.java
@@ -38,4 +38,9 @@ public class MainActivity extends ReactActivity {
             }
         };
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+    }    
 }
