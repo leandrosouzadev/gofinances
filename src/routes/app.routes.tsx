af4e8be050505750c1b429116d9a67f3afa6e54a
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2021-10-03 10:03:29 -0300

    Salvando novas transações

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Platform } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { useTheme } from 'styled-components';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator, BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
@@ -18,14 +18,16 @@ export function AppRoutes() {
                 tabBarActiveTintColor: theme.colors.secundary,
                 tabBarInactiveTintColor: theme.colors.text,
                 tabBarLabelPosition: 'beside-icon',
+                headerShown: false,                
                 tabBarStyle: {
                     paddingVertical: Platform.OS === 'ios' ? 20 : 0,
                     height: 88
                 }
             }}
         >
-            <Screen 
-                name="Listagem"
+            <Screen                 
+                key="Listagem"
+                name="Listagem"                
                 component={Dashboard}
                 options={{
                     tabBarIcon: (({ size, color }) =>(
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     Modal,
     TouchableWithoutFeedback,
@@ -7,8 +7,11 @@ import {
 } from 'react-native';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid';
 
 import { useForm } from 'react-hook-form';
+import { useNavigation } from '@react-navigation/native';
 
 import { InputForm } from '../../components/Form/InputForm';
 import { Button } from '../../components/Form/Button';
@@ -46,14 +49,19 @@ export function Register() {
     const [transactionType, setTransactionType] = useState('');
     const [categoryModalOpen, setCategoryModalOpen] = useState(false);
 
+    const dataKey = '@gofinances:transactions';
+
     const [category, setCategory] = useState({
         key: 'category',
         name: 'Categoria',        
     });
 
+    const navigation = useNavigation();
+
     const {
         control,
         handleSubmit,
+        reset,
         formState: { errors }
     } = useForm({
         resolver: yupResolver(schema)
@@ -71,21 +79,45 @@ export function Register() {
         setCategoryModalOpen(false);
     }
 
-    function handleRegister(form: FormData) {
+    async function handleRegister(form: FormData) {
         if(!transactionType)
             return Alert.alert('Selecione o tipo da transação');
 
         if(category.key === 'category')
             return Alert.alert('Selecione a categoria');
 
-        const data = {
+        const newTransaction = {
+            id: String(uuid.v4()),
             name: form.name,
             amount: form.amount,
             transactionType,
-            category: category.key
+            category: category.key,
+            date: new Date()
         }
 
-        console.log(data);
+        try {         
+            const data = await AsyncStorage.getItem(dataKey);
+            const currentData = data ? JSON.parse(data) : [];
+
+            const dataFormatted = [
+                ...currentData,
+                newTransaction
+            ]
+
+            await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+
+            reset();
+            setTransactionType('');
+            setCategory({
+                key: 'category',
+                name: 'Categoria'
+            });
+
+            navigation.navigate('Listagem' as never);
+        } catch (error) {
+            console.log(error);
+            Alert.alert("Não foi possivel salvar");
+        }
     }
 
     return(
