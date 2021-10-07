d7aeb96e67ef865f28081ddec8ee46ea777a4b4a
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2021-10-07 05:49:26 -0300

    Preparando os dados para o gráfico

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -25,6 +25,8 @@ interface CategoryData {
     name: string;
     total: string;
     color: string;
+    percentFormatted: string;
+    percent: number;
 }
 
 export function Resume() {
@@ -37,6 +39,11 @@ export function Resume() {
         
         const expensives = responseFormatted
         .filter((expensive: TransactionData) => expensive.type === 'negative');
+
+        const expensivesTotal = expensives
+        .reduce((accumulator: number, expensive: TransactionData) => {
+            return accumulator + Number(expensive.amount);
+        }, 0);        
 
         const totalByCategory: CategoryData[] = [];
 
@@ -55,12 +62,17 @@ export function Resume() {
                     currency: 'BRL'
                 });
 
+                const percent = (categorySum / expensivesTotal * 100);
+                const percentFormatted = `${percent.toFixed(0)}%`;
+
                 totalByCategory.push({
                     key: category.key,
                     name: category.name,
                     color: category.color,
-                    total
-                });
+                    total,
+                    percent,
+                    percentFormatted
+                });                
             }
         });
 
